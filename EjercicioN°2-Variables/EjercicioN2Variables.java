dd0ee7ddb17b956716dc7be3b22afdd5ce8d8b7f
notsag01 <79101798+notsag01@users.noreply.github.com>
2023-04-04 16:52:50 -0300

    Ejercicio N°2

---
+++ EjercicioN°2-Variables/EjercicioN2Variables.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicion.pkg2.variables;
+
+/**
+ *
+ * @author Administrador
+ */
+public class EjercicioN2Variables {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*Realizar un programa que utilice tres variables con valores
+        instanciados. Luego realizar la operación de suma con esas dos 
+        variables y guardar el resultado en la tercera variable. 
+        Para finalizar imprimir mensaje con el resultado.*/
+        
+        int numA= 5;
+        int numB=6;
+        int suma = numA + numB;
+        
+        System.out.println("La suma de "+ numA +" + " + numB +" es igual a " +suma);
+        
+        
+    }
+    
+}
