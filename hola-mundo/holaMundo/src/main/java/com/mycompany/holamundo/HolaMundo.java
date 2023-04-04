4476198a53d4b4e987df7cdae7d84e45d5bc46c0
notsag01 <79101798+notsag01@users.noreply.github.com>
2023-04-04 09:40:46 -0300

    holaMundo

---
+++ hola-mundo/holaMundo/src/main/java/com/mycompany/holamundo/HolaMundo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.holamundo;
+
+/**
+ *
+ * @author Administrador
+ */
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");
+    }
+}
