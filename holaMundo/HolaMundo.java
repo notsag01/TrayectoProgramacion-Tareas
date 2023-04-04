0fa02fd4d6d66bce17268cc06de933fe7c270952
notsag01 <79101798+notsag01@users.noreply.github.com>
2023-04-04 15:33:17 -0300

    hola mundo

--- holaMundo/HolaMundo.java
+++ holaMundo/HolaMundo.java
@@ -1,8 +1,8 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-
-package com.mycompany.holamundo;
+package holamundo;
 
 /**
  *
@@ -10,7 +10,11 @@ package com.mycompany.holamundo;
  */
 public class HolaMundo {
 
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        System.out.println("Hola mundo!");
+        System.out.println("Hola Mundo");
     }
+    
 }
