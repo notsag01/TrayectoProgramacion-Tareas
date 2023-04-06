465834ba32867d9a30c9c425aca05e7654deaac6
notsag01 <gcordoba1981@gmail.com>
2023-04-06 17:24:03 -0300

    Ejercicio3

---
+++ Ejecrcio N°3 Variables-Constantes-Tipos/src/ejecrcio/n/pkg3/variables/constantes/tipos/EjecrcioN3VariablesConstantesTipos.java
@@ -0,0 +1,77 @@
+
+package ejecrcio.n.pkg3.variables.constantes.tipos;
+
+public class EjecrcioN3VariablesConstantesTipos {
+
+    
+    public static void main(String[] args) {
+        /*// Variables
+int numero1;
+int numero2;
+int suma;
+int edad;
+String nombre;
+// Constante
+final int NUMERO_3= 4;
+// Ingreso los valores a la variables numero1 y numero2
+numero1 = 30;
+numero2 = 40;
+// operación suma
+XXXXXXXXXXXXXXXXXXXXXXXXX
+// Muestro en pantalla las dos variables y el resultado de la suma
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+// Cambio los valores de numero1,numero2 y vuelvo a realizar la operación de suma
+numero1=100;
+XXXXXXXXXX
+//operación suma
+XXXXXXXXXX
+// Muestro en pantalla las dos variables y el resultado de la suma
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+// Instancio y muestro en pantalla la variable nombre y su edad
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+// Muestro en pantalla la constante
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx
+
+}
+
+}*/
+        
+        
+//RESOLUCION ------------------------------------------
+// Variables
+int numero1;
+int numero2;
+int suma;
+int edad;
+String nombre;
+// Constante
+final int NUMERO_3= 4;
+// Ingreso los valores a la variables numero1 y numero2
+numero1 = 30;
+numero2 = 40;
+// operación suma
+suma = numero1 + numero2;
+// Muestro en pantalla las dos variables y el resultado de la suma
+System.out.println("La suma de " + numero1 + " mas " + numero2 + " es igual a " + suma );
+// Cambio los valores de numero1,numero2 y vuelvo a realizar la operación de suma
+numero1=100;
+numero2=200;
+//operación suma
+suma = numero1 + numero2;
+// Muestro en pantalla las dos variables y el resultado de la suma
+System.out.println("La suma de " + numero1 + " mas " + numero2 + " es igual a " + suma );
+// Instancio y muestro en pantalla la variable nombre y su edad
+nombre="Gastón";
+edad= 40;
+System.out.println(nombre + edad + " años");
+// Muestro en pantalla la constante
+System.out.println(NUMERO_3);
+
+//FIN DE LA RESOLUCIÓN-------------------------------------------------------------------------------------------
+
+    }
+    
+}
