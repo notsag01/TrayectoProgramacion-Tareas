481b9c5ad06f5ade5afea3d3684e6ccc8162410c
notsag01 <79101798+notsag01@users.noreply.github.com>
2023-04-13 16:36:42 -0300

    Ejercicio 6

---
+++ Ejercicio N°6 Condicionales IF/src/ejercicio/n/pkg6/condicionales/pkgif/EjercicioN6CondicionalesIF.java
@@ -0,0 +1,123 @@
+
+package ejercicio.n.pkg6.condicionales.pkgif;
+
+
+public class EjercicioN6CondicionalesIF {
+
+    public static void main(String[] args) {
+        //Ejercicio 1;
+        
+//            int temperatura = 27;
+//
+//            if(temperatura > 25){
+//                System.out.println("A la playa!");
+//        }
+
+        //****************************************
+        
+        //EJERCICIO 2
+//        boolean haceSol=true;
+//        
+//        if(haceSol){
+//            System.out.println("No te olvides de la sombrilla");
+//        }
+
+        //*****************************************
+        
+        //EJERCICIO 3
+        
+//        boolean haceSol=true;
+//        boolean nevado=true;
+//        
+//        if(haceSol || nevado ){
+//            System.out.println("Qué bien!");
+//        }
+
+        //*****************************************
+        
+        //EJERCICIO 4
+        
+//        boolean nevado= true;
+//        int temperatura= 29;
+//        
+//        if ( nevado && temperatura >= 20 && temperatura <=30){
+//            System.out.println("No me la creo");
+//        }
+
+        //*****************************************
+        
+        //EJERCICIO 5
+        
+//        int temperatura = 32;
+//        boolean haceSol= true;
+//        
+//        if(temperatura < 0 || temperatura >30 && haceSol){
+//            System.out.println("Mejor me quedo en casa");
+//        }
+        
+        //*****************************************
+        
+        //EJERCICIO 6
+        
+//        int temperatura = 32;
+//
+//            if(temperatura > 25){
+//                System.out.println("A la playa!");
+//        }
+
+        //*****************************************
+        
+        //EJERCICIO 7
+        
+//        int temperatura= 10;
+//        
+//        if(temperatura <= 25){
+//            System.out.println("Esperando al buen tiempo");
+//        }
+
+        //******************************************
+        
+        //EJERCICIO 8
+        
+//        int temperatura = 10;
+//        
+//        if(temperatura >25){
+//            System.out.println("A la playa!");
+//        }else{
+//            System.out.println(" Esperando al buen tiempo");
+//        }
+
+        //*****************************************
+        
+        //EJERCICIO 9
+        
+//        int temperatura = 10;
+//        boolean nevado= true;
+//        
+//        if(temperatura >= 25){
+//            System.out.println("A la playa");
+//        }else if(temperatura >= 15){
+//            System.out.println("A la montaña");
+//        }else if(temperatura >= 5 && nevado){
+//            System.out.println("A esquiar");
+//        }else{
+//            System.out.println("A dormit");
+//        }
+
+        //*****************************************
+        
+        // EJERCICIO 10
+        
+        int temperatura = 10;
+        
+        if( tempearatura < 5){
+            System.out.println("A estquiar");
+        }else{
+            System.out.println(" A Dormir");
+        }
+    
+
+
+    }
+    
+}
