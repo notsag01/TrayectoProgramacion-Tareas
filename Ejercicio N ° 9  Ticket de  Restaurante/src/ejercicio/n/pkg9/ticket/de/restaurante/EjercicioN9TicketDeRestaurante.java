ee3c801159e9da48b7e08985752ee7aee60f1700
notsag01 <79101798+notsag01@users.noreply.github.com>
2023-04-20 17:04:51 -0300

    Ejercicio 9 Tiket

--- Ejercicio N ° 9  Ticket de  Restaurante/src/ejercicio/n/pkg9/ticket/de/restaurante/EjercicioN9TicketDeRestaurante.java
+++ Ejercicio N ° 9  Ticket de  Restaurante/src/ejercicio/n/pkg9/ticket/de/restaurante/EjercicioN9TicketDeRestaurante.java
@@ -12,28 +12,22 @@ public class EjercicioN9TicketDeRestaurante {
         
         String nombreMozo;
 
-        double totalCosunmido;
+        double subtotal;
+        double total;
         int formaDePago;
-        int cantidaCuotas;
-        double descuentoEfectivo= 0.3;
-        double descuentoDebito= 0.1;
-        double recargoCredito= 0.1;
-        double gaseosa=100;
-        double milanesa=100;
-        double flan=100;
-        double cafe=100;
-        double subtotal= gaseosa + milanesa + flan + cafe;
+        int numeroTarjeta;
         
         System.out.print("Ingrese el nombre del mozo: ");
         nombreMozo=datos.nextLine();
+        System.out.print("Ingrese el subtotal: ");
+        subtotal=datos.nextFloat();
         
-        System.out.println(" INGRESE LA FORMA DE PAGO");
+         System.out.println("FORMA DE PAGO");
         System.out.println(" 1: EFECTIVO");
         System.out.println(" 2: TARJETA DE DEBITO");
         System.out.println(" 3: TARJETA DE CREDITO");
+        System.out.print(" INGRESE LA FORMA DE PAGO: ");
         formaDePago = datos.nextInt();
-        
-        
         
         System.out.println("------------------");
         System.out.println("------------------");
@@ -44,21 +38,49 @@ public class EjercicioN9TicketDeRestaurante {
         System.out.print("CAJA 1  ");
         System.out.println("TICKET 0001");
         System.out.print("LO ATENDIO: ");
-        System.out.print(nombreMozo);
+        System.out.println(nombreMozo);
         System.out.println("------------------");
         System.out.println("------------------");
-        System.out.println("1 GASEOSA");
-        System.out.println("1 MILANESA NAPOLITANA CON FRITAS");
-        System.out.println("1 FLAN CON CREMAS");
-        System.out.println("1 CAFE");
-        System.out.println("------------------");
-        System.out.println("------------------");
-        System.out.print("SUBTOTAL: $");
-        System.out.println(subtotal);
-        System.out.print("FORMA DE PAGO");
-//        if(formaDePago== 1){
-//            System.out.println("EFECTIVO");
-//        }
+        System.out.println("Subtotal: $" + subtotal);
+        
+        if(formaDePago ==1 ){
+            System.out.println("Forma de pago: EFECTIVO");
+            System.out.println("Descuento: 30%");
+            total= (subtotal / 1.3);
+            System.out.println("TOTAL: $" + total);
+            System.out.println("----------------------");
+            System.out.println("GRACIAS POR SU COMPRA.");
+            System.out.println("----------------------");
+             
+        }else if(formaDePago == 2){
+            System.out.println("Forma de pago: DEBITO");        
+            System.out.println("Recargo: 10%");      
+            total=(subtotal * 1.1);
+            System.out.println("TOTAL: $" + total);
+            System.out.print("Ingres el número de su tarjeta: ");
+            numeroTarjeta= datos.nextInt();
+            System.out.print("Ingres el número de cuotas: ");
+            System.out.println("----------------------");
+            System.out.println("GRACIAS POR SU COMPRA.");
+            System.out.println("----------------------");     
+            
+        }else{
+            System.out.println("Forma de pago: CREDITO");        
+            System.out.println("Recargo: 30%");    
+            total=(subtotal * 1.3);
+            System.out.println("TOTAL: $" + total);
+            System.out.print("Ingres el número de su tarjeta: ");
+            numeroTarjeta= datos.nextInt();
+            System.out.print("Ingres el número de cuotas: ");
+            int cantidadCuotas = datos.nextInt();
+            System.out.println("----------------------");
+            System.out.println("GRACIAS POR SU COMPRA.");
+            System.out.println("----------------------");  
+        }
+        
+
+       
+        
         
         
 
