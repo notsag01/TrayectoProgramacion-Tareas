6dd002d244217edf6e8ebfe0f944b03c134f743b
notsag01 <79101798+notsag01@users.noreply.github.com>
2023-04-20 15:29:26 -0300

    EJERCICIO 10 SWITCH

--- Ejercicio N ° 10 - SWITCH/src/ejercicio/n/pkg10/pkgswitch/EjercicioN10SWITCH.java
+++ Ejercicio N ° 10 - SWITCH/src/ejercicio/n/pkg10/pkgswitch/EjercicioN10SWITCH.java
@@ -19,25 +19,42 @@ public class EjercicioN10SWITCH {
         System.out.print("Ingrese un importe: ");
         importe = leer.nextDouble();
         
-        System.out.println("-- Ingrese método de pago: ");
         System.out.println("-- 1 Efectivo ------------ ");
         System.out.println("-- 2 Tarjeta de Crédito  - ");
         System.out.println("-- 3 Tarjeta de débito  -- ");
+        System.out.print("---- Ingrese método de pago: ");
         formaPago= leerFormaPago.nextInt();
         
         switch(formaPago){
             case 1:
                 total= importe / 1.4;
-                System.out.println(total);
+                System.out.println("-------------------------"      );
+                System.out.println("Importe: $" + importe +"---"      );
+                System.out.println("Medio de pago: EFECTIVO "       );
+                System.out.println("DESCUENTO 10%" );
+                System.out.println("TOTAL: $" + total );
+                System.out.println("GRACIAS POR SU COMPRA ---------");
                 break;
             case 2:
                 total= importe * 1.2;
-                System.out.println(total);
+                System.out.println("Importe: $" + importe +"----------");
+                System.out.println("Medio de pago: TARJETA CRÉDITO " );
+                System.out.println("RECARGO 20%" );
+                System.out.println("TOTAL: $" + total );
                 System.out.print("Ingrese el numero de tarjeta: ");
                 numeroTarjeta= leerFormaPago.nextInt();
+                System.out.println("GRACIAS POR SU COMPRA ---");
+            break;    
             case 3:
-                
+                total = importe * 1.1;
+                System.out.println("Importe: $" + importe +"----------");
+                System.out.println("Medio de pago: TARJETA DÉBITO" );
+                System.out.println("RECARGO 10%" );
+                System.out.println("TOTAL: $" + total );
+                System.out.println("GRACIAS POR SU COMPRA ---");
             default:
+                System.out.println("LA OPCIÓN ELEGIDA NO ES CORRECTA.");
+                System.out.println("POR FAVOR, VUELVA A INTENTARLO.");
             break;
         }
     }
