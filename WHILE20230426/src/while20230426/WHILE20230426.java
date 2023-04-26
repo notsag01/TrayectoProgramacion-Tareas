131a5a505397102f6fe2755e3cf0daef12a2ea20
notsag01 <gcordoba1981@gmail.com>
2023-04-26 20:11:40 -0300

    WHILE

---
+++ WHILE20230426/src/while20230426/WHILE20230426.java
@@ -0,0 +1,37 @@
+
+package while20230426;
+
+import java.util.Scanner;
+
+
+public class WHILE20230426 {
+
+   
+    public static void main(String[] args) {
+       Scanner leer=new Scanner(System.in);
+       int contador=0;
+//       while(contador<10){
+//           System.out.println(contador);
+//           contador ++;
+//       }
+       
+//       do{
+//            System.out.println(contador);
+//            contador ++;
+//        }while(contador<10);
+        
+        String usuario="";
+        
+        while(contador <= 3){
+        System.out.print("Ingrese el usuario: ");
+        usuario= leer.nextLine();
+        if(usuario.equals("pepe")){
+            System.out.println("Usuario Correcto");
+        }else{
+            System.out.println("Usuario Incorrecto");
+        }
+        contador++;
+        }
+    }
+    
+}
