1fb86dab86c4c68c0d9ddba918da71a4498ef40d
notsag01 <79101798+notsag01@users.noreply.github.com>
2023-05-10 17:01:38 -0300

    SERVICIOS AUTOMOTORES2

--- SERVICIO AUMOTMORES/src/servicio/aumotmores/SERVICIOAUMOTMORES.java
+++ SERVICIO AUMOTMORES/src/servicio/aumotmores/SERVICIOAUMOTMORES.java
@@ -42,122 +42,94 @@ public class SERVICIOAUMOTMORES {
 //******************************************************************************      
      
        
-        //VARIABLES
+        //VARIABLES***
         Scanner datos= new Scanner(System.in);
         Random nRandom= new Random();
        
-        //VARIABLES
-        String tickets [][]= new String [3] [6];
+        //ARRAYS***
+        String [] nombres=new String[1];
+        String [] telefonos=new String[1];
+        String [] dniS=new String[1];
+        int [] nOrden=new int[1];
+        String [] dominios=new String[1];
+        String [] marcas=new String[1];
+        String [] servicios1=new String[1];
+        String [] cantidadCubiertas= new String[1];
+        String [] servicios2=new String[1];
+        String [] servicios3=new String[1];
+        String [] servicios4=new String[1];
         
-        tickets[0][0]="NOMBRE";
-        tickets[0][1]="TELEFNO";
-        tickets[0][5]="DNI";
-        tickets[0][2]="DOMINIO";
-        tickets[0][3]="MARCA";
-        tickets[0][4]="SERVICIO1";
+        //CONTADORES**
         
-        
-        String nombre= " ";
-        String dni=" ";
-        String telefono=" ";
-        String dominio=" ";
-        String marca="";
-        int servicio;
-        String servicios ="";
-        int numeroEspera=nRandom.nextInt(100)+1;
-       
-        System.out.print("nombre: ");
-        nombre=datos.nextLine();
-        tickets[1][0]= nombre;
-        System.out.print("Numero DNI: ");
-        dni=datos.nextLine();
-        tickets[1][5]=dni;
-        System.out.print("telefono: ");
-        telefono=datos.nextLine();
-        tickets[1][1]= telefono;
-        System.out.print("dominio: ");
-        dominio=datos.nextLine();
-        tickets [1][2]= dominio;
-        
-        
-                  
-        System.out.println("***   MARCA   ***");
-        System.out.println("1- FORD    ");
-        System.out.println("2- PEUGEOT      ");
-        System.out.println("3- RENAULT       ");
-        System.out.println("4- NISSAN     ");
-        System.out.println("5- FIAT     ");
-        System.out.print("LA MARCA DEL AUTO((numero): ");
-        marca=datos.nextLine();
-       
-        switch (marca){
-            case "1" : tickets[1][3]= "FORD";
-            break;
-            case "2": tickets[1][3]="PEUGEOT";
-            break;
-            case "3": tickets[1][3]=" RENAULT";
-            break;
-            case "4": tickets[1][3]=" NISSAN";
-            break;
-            default: System.out.print("no ha elegido un servicio.");
-            break;
+        for(int i=0; i<nombres.length;i++){
+            System.out.print("Ingrese su nombre: ");
+            String nombre= datos.nextLine();
+            nombres[i]=nombre;
+            
+            System.out.print("Ingrese su telefono: ");
+            String telefono= datos.nextLine();
+            telefonos[i]=telefono;
+            
+            System.out.print("Ingrese su DNI: ");
+            String dni= datos.nextLine();
+            dniS[i]=dni;
+            
+            int orden=nRandom.nextInt(100) + 1;
+            nOrden[i]=orden;
+            
+            System.out.println("***   MARCA   ***");
+            System.out.println("1- FORD    ");
+            System.out.println("2- PEUGEOT      ");
+            System.out.println("3- RENAULT       ");
+            System.out.println("4- NISSAN     ");
+            System.out.println("5- FIAT     ");
+            System.out.print("LA MARCA DEL AUTO((numero): ");
+            String marca=datos.nextLine();
+            
+            switch (marca){
+                case "1" : marcas[i]= "FORD";
+                break;
+                case "2": marcas[i]="PEUGEOT";
+                break;
+                case "3": marcas[i]=" RENAULT";
+                break;
+                case "4": marcas[i]=" NISSAN";
+                break;
+                case "5": marcas[i]=" FIAT";
+                break;
+                default: System.out.print("no ha elegido un servicio.");
+                break;
+            }
+            
+            System.out.println("***   SERVICIOS   ***");
+            System.out.println("1- ALINEACION    ");
+            System.out.println("2- BALANCEO      ");
+            System.out.println("3- ACEITE        ");
+            System.out.println("4- CUBIERTAS     ");
+            System.out.print("Elija un servicio((numero): ");
+            String servicio=datos.nextLine();
+            
+             switch (servicio){
+                case "1" : servicios1[i]= "ALINEACION";
+                break;
+                case "2": servicios1[i]="BALANCEO";
+                          System.out.print("Cantidad: ");
+                          String cantCubiertas="";
+                          cantCubiertas=datos.nextLine();
+                          cantidadCubiertas[i]=cantCubiertas;
+                          break;
+                          case "3": servicios1[i]=" ACEITE";
+                          break;
+                          case "4": servicios1[i]=" CUBIERTAS";
+                          break;
+                          default: System.out.print("no ha elegido un servicio.");
+                          break;
+            }
+            
+            System.out.println(nombres[i]+ "- " + telefonos[i] + "- " + dniS[i]
+                                +"- " + nOrden[i] + "- "+ marcas[i] + "- " + servicios1[i] + "-" + cantidadCubiertas[i] );
         }
         
-       
-        System.out.println("***   SERVICIOS   ***");
-        System.out.println("1- ALINEACION    ");
-        System.out.println("2- BALANCEO      ");
-        System.out.println("3- ACEITE        ");
-        System.out.println("4- CUBIERTAS     ");
-        System.out.print("Elija un servicio((numero): ");
-        servicio=datos.nextInt();
-        
-       
-       
-        switch (servicio){
-            case 1 : tickets[1][4]= "ALINEACION";
-            break;
-            case 2: 
-                    tickets[1][4]="BALANCEO";
-                    String cantidadCubiertas="";
-                    System.out.print("Cantidad: ");
-                    cantidadCubiertas=datos.nextLine();
-                    tickets[2][4]=cantidadCubiertas;
-            break;
-            case 3: tickets[1][4]=" ACEITE";
-            break;
-            case 4: tickets[1][4]=" CUBIERTAS";
-            break;
-            default: System.out.print("no ha elegido un servicio.");
-            break;
-        }
-        
-        
-        
-        
-//        for(int f=0; f<tickets.length;f++){
-//            for(int c=0; c<tickets[f].length;c++){
-//                System.out.println(tickets[f][c]);
-//            }            
-//        }
-        
-          System.out.println("\n==================================");
-          System.out.println("======== Bienvenido ==============");
-          System.out.println("Centro de Servicios Automotor CFP N°36\nZavaleta 204, C1437EYF, CABA");
-          System.out.println("=====Nº ORDEN: " + numeroEspera + "===========================");
-          System.out.println("Nombre del Cliente: " + tickets[1][0]);
-          System.out.println("DNI del cliente: " + tickets[1][5]);
-          System.out.println("Marca del Vehiculo: " + tickets[1][3]);
-          System.out.println("Dominio del Vehiculo: " + tickets[1][2]);
-          System.out.println("==================================");
-//          System.out.println("Cantidad de servicios: " + cont);
-//       
-//System.out.println("Los servicios son : " + cont2);
-//        System.out.println("==================================");
-        
-//******************************************************************************      
-//******************************************************************************      
-
        
 
     }
