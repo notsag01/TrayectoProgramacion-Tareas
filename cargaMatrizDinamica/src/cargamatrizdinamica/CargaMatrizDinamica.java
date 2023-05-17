95051ec637a287c1cf0f0c0b29ec145d7a8220fa
notsag01 <79101798+notsag01@users.noreply.github.com>
2023-05-17 12:38:40 -0300

    matrices dinamicas

--- cargaMatrizDinamica/src/cargamatrizdinamica/CargaMatrizDinamica.java
+++ cargaMatrizDinamica/src/cargamatrizdinamica/CargaMatrizDinamica.java
@@ -11,22 +11,35 @@ public class CargaMatrizDinamica {
         int matriz[][] = new int[3][3];
         
         Scanner consola = new Scanner(System.in);
+//        
+//     //cargar matriz dinamica con usuario  
+//        for (int x=0; x < matriz.length; x++) {
+//            for (int y=0; y < matriz[x].length; y++) {
+//            System.out.print("Introduzca un numero a la matriz[" + x + "," + y + "]  ");
+//            matriz[x][y] = consola.nextInt();
+//        }
+//        }
+//        // mostrar matriz
+//        for (int[] matriz1 : matriz) {
+//            for (int y = 0; y < matriz1.length; y++) {
+//                //System.out.println ("[" + x + "," + y + "] = " + matriz[x][y]);
+//                System.out.print(matriz1[y]);
+//                System.out.print("-");
+//            }
+//        }
+
+        String nombres[][]= new String [3][3];
         
-     //cargar matriz dinamica con usuario  
-        for (int x=0; x < matriz.length; x++) {
-            for (int y=0; y < matriz[x].length; y++) {
-            System.out.print("Introduzca un numero a la matriz[" + x + "," + y + "]  ");
-            matriz[x][y] = consola.nextInt();
-        }
-        }
-        // mostrar matriz
-        for (int[] matriz1 : matriz) {
-            for (int y = 0; y < matriz1.length; y++) {
-                //System.out.println ("[" + x + "," + y + "] = " + matriz[x][y]);
-                System.out.print(matriz1[y]);
+        for(int x=0; x<nombres.length; x++){
+            for(int y=0; y<nombres[x].length; y++){
+                System.out.print(nombres[x][y]);
                 System.out.print("-");
             }
         }
+
+
+
+
     }
     
 }
