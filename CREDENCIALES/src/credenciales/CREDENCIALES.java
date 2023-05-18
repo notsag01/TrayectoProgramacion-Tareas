eb62bf843ddcb01f8aa9cd190073efd6e99bf740
notsag01 <79101798+notsag01@users.noreply.github.com>
2023-05-18 16:12:05 -0300

    CREDENCIALES

--- CREDENCIALES/src/credenciales/CREDENCIALES.java
+++ CREDENCIALES/src/credenciales/CREDENCIALES.java
@@ -11,23 +11,30 @@ public class CREDENCIALES {
         final String id="123A";
         String usuarioVerificar="";
         String idVerificar="";
+        int contador=0;
         
-        usuarioVerificar= JOptionPane.showInputDialog(null,"Ingrese su nombre ","INPUT",JOptionPane.QUESTION_MESSAGE).toUpperCase();        
-        System.out.println(usuarioVerificar);
-        idVerificar = JOptionPane.showInputDialog(null,"Ingrese su nombre ","INPUT",JOptionPane.QUESTION_MESSAGE).toUpperCase();        
-        System.out.println(idVerificar);
-        
-        if(usuario.equals(usuarioVerificar) && id.equals(idVerificar) ){
-            JOptionPane.showMessageDialog(null,"ACCESO ","Mensaje de Informacion",JOptionPane.INFORMATION_MESSAGE);
-        }else if(usuario.equals(usuarioVerificar)  && !id.equals(idVerificar)){
-            JOptionPane.showMessageDialog(null,"ACCESO DENEGADO \n El ID es incorrecto" ,"Mensaje de Informacion",JOptionPane.INFORMATION_MESSAGE);                    
-        }else if(!usuario.equals(usuarioVerificar)  && id.equals(idVerificar)){
-            JOptionPane.showMessageDialog(null,"ACCESO DENEGADO \n El Usuario es incorrecto" ,"Mensaje de Informacion",JOptionPane.INFORMATION_MESSAGE);            
-        }else{
-            JOptionPane.showMessageDialog(null,"ACCESO DENEGADO \n Verifique sus credenciales" ,"Mensaje de Informacion",JOptionPane.INFORMATION_MESSAGE);            
-            
+        while( contador < 3 && (!usuario.equals(usuarioVerificar)  || !id.equals(idVerificar))){
+            usuarioVerificar= JOptionPane.showInputDialog(null,"Ingrese su nombre ","INPUT",JOptionPane.QUESTION_MESSAGE).toUpperCase();        
+            System.out.println(usuarioVerificar);
+            idVerificar = JOptionPane.showInputDialog(null,"Ingrese su nombre ","INPUT",JOptionPane.QUESTION_MESSAGE).toUpperCase();        
+            System.out.println(idVerificar);
+
+            if(usuario.equals(usuarioVerificar) && id.equals(idVerificar) ){
+                JOptionPane.showMessageDialog(null,"ACCESO ","Mensaje de Informacion",JOptionPane.INFORMATION_MESSAGE);
+            }else if(usuario.equals(usuarioVerificar)  && !id.equals(idVerificar)){
+                JOptionPane.showMessageDialog(null,"ACCESO DENEGADO \n El ID es incorrecto" ,"Mensaje de Informacion",JOptionPane.INFORMATION_MESSAGE);                    
+                contador++;
+                System.out.println(contador);
+            }else if(!usuario.equals(usuarioVerificar)  && id.equals(idVerificar)){
+                JOptionPane.showMessageDialog(null,"ACCESO DENEGADO \n El Usuario es incorrecto" ,"Mensaje de Informacion",JOptionPane.INFORMATION_MESSAGE);            
+                contador++;
+                System.out.println(contador);
+            }else{
+                JOptionPane.showMessageDialog(null,"ACCESO DENEGADO \n Verifique sus credenciales" ,"Mensaje de Informacion",JOptionPane.INFORMATION_MESSAGE);                        
+                contador++;
+                System.out.println(contador);
+            }
         }
-        
         
         
         
