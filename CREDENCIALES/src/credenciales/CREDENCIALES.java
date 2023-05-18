211f7cc0e0bb114ad0bcf8747487b08326420a3e
notsag01 <79101798+notsag01@users.noreply.github.com>
2023-05-18 15:54:04 -0300

    CREDENCIALES

--- CREDENCIALES/src/credenciales/CREDENCIALES.java
+++ CREDENCIALES/src/credenciales/CREDENCIALES.java
@@ -1,34 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package credenciales;
 
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author gas12
- */
 public class CREDENCIALES {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        String nombre="GASTON";
-        String id="1234";
-        String nombreVerificar="";
-        String idVerificar;
+        final String usuario="GASTON";
+        final String id="123A";
+        String usuarioVerificar="";
+        String idVerificar="";
         
-        
-        nombreVerificar= JOptionPane.showInputDialog(null,"Ingrese su nombre ","INPUT",JOptionPane.QUESTION_MESSAGE).toUpperCase();        
-        System.out.println(nombreVerificar);
-        idVerificar = JOptionPane.showInputDialog(null,"Ingrese su nombre ","INPUT",JOptionPane.QUESTION_MESSAGE);        
+        usuarioVerificar= JOptionPane.showInputDialog(null,"Ingrese su nombre ","INPUT",JOptionPane.QUESTION_MESSAGE).toUpperCase();        
+        System.out.println(usuarioVerificar);
+        idVerificar = JOptionPane.showInputDialog(null,"Ingrese su nombre ","INPUT",JOptionPane.QUESTION_MESSAGE).toUpperCase();        
         System.out.println(idVerificar);
         
-        if(nombre.equals(nombreVerificar)){
+        if(usuario.equals(usuarioVerificar) && id.equals(idVerificar) ){
             JOptionPane.showMessageDialog(null,"ACCESO ","Mensaje de Informacion",JOptionPane.INFORMATION_MESSAGE);
+        }else if(usuario.equals(usuarioVerificar)  && !id.equals(idVerificar)){
+            JOptionPane.showMessageDialog(null,"ACCESO DENEGADO \n El ID es incorrecto" ,"Mensaje de Informacion",JOptionPane.INFORMATION_MESSAGE);                    
+        }else if(!usuario.equals(usuarioVerificar)  && id.equals(idVerificar)){
+            JOptionPane.showMessageDialog(null,"ACCESO DENEGADO \n El Usuario es incorrecto" ,"Mensaje de Informacion",JOptionPane.INFORMATION_MESSAGE);            
+        }else{
+            JOptionPane.showMessageDialog(null,"ACCESO DENEGADO \n Verifique sus credenciales" ,"Mensaje de Informacion",JOptionPane.INFORMATION_MESSAGE);            
+            
         }
         
         
