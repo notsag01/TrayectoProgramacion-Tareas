532c0a5a10363c23439df095ab9dad85cbc04b43
notsag01 <79101798+notsag01@users.noreply.github.com>
2023-05-22 14:49:12 -0300

    JUEGO DEL VASO version2

--- JUEGO VASO/src/juego/vaso/JUEGOVASO.java
+++ JUEGO VASO/src/juego/vaso/JUEGOVASO.java
@@ -48,80 +48,90 @@ public class JUEGOVASO {
         ImageIcon Bien = new ImageIcon("src/imagenes/bien.png");
         ImageIcon Mal = new ImageIcon("src/imagenes/mal.png");
         
-        jugadores[0]= JOptionPane.showInputDialog(null,"JUGADOR Nª1","Juego del Vaso",JOptionPane.QUESTION_MESSAGE).toUpperCase();
-        jugadores[1]= JOptionPane.showInputDialog(null,"JUGADOR Nª2","Juego del Vaso",JOptionPane.QUESTION_MESSAGE).toUpperCase();
-        //System.out.println(jugadores[0] + jugadores[1]);
+        JOptionPane.showMessageDialog(null, "Bienvenidos al JUEGO DEL VASO", "BIENVENIDA", JOptionPane.INFORMATION_MESSAGE , Icono);
+        int modo = JOptionPane.showOptionDialog(null, "Eligue una opcione", "El juego del Vaso",
+                                     JOptionPane.YES_NO_CANCEL_OPTION,
+                                     JOptionPane.INFORMATION_MESSAGE, Icono,
+                                     new Object[]{"COMPUTADORA", "MULTIJUGADOR"}, "COMPUTADORA");
         
-        JOptionPane.showMessageDialog(null, "JUGUEMOS!", "Juego del Vaso", JOptionPane.INFORMATION_MESSAGE, Icono);
-        
-        for(int i=0; i<contadorVueltas; i++ ){
-             
-            //JUGAS DEL JUGADOR 1 ----------------------------------
-        int seleccionJ1 = JOptionPane.showOptionDialog(null,
-                jugadores[0] + ", \n encuentra la pelotita en los vasos!",
-                "JUGADOR 1",
-                JOptionPane.YES_NO_CANCEL_OPTION,
-                JOptionPane.INFORMATION_MESSAGE, Icono,
-                new Object[]{"VASO 1", "VASO 2", "VASO 3"}, "VASO1");                
-            jugadasJugador1[i]=seleccionJ1;            
-            
-            //ACIERTOS JUGADOR 1
-            if(pelotita == seleccionJ1){
-                contadorPuntosJ1++;
-            }
-            
-            //JUGAS DEL JUGADOR 2 --------------------------------------
-        int seleccionJ2 = JOptionPane.showOptionDialog(null,
-                jugadores[1] + ", \n encuentra la pelotita en los vasos!",
-                "JUGADOR 2",
-                JOptionPane.YES_NO_CANCEL_OPTION,
-                JOptionPane.INFORMATION_MESSAGE, Icono,
-                new Object[]{"VASO 1", "VASO 2", "VASO 3"}, "VASO1");                
-            jugadasJugador2[i]=seleccionJ2;   
-            
-            //ACIERTOS JUGADOR 2
-            if(pelotita == seleccionJ2){
-                contadorPuntosJ2++;
-            }
-            
-            //NOS FIJAMOS QUIÉN ACERTÓ Y LO MOSTRAMOS POR PANTALLA
-            if(pelotita== seleccionJ1 && pelotita==seleccionJ2){
-                JOptionPane.showMessageDialog(null, "Felicitaciones! \n Ambos han Acertado! \n Punto para los dos", "Juego del Vaso",
-                        JOptionPane.INFORMATION_MESSAGE, Bien);
-            }else if(pelotita== seleccionJ1 && (pelotita <seleccionJ2|| pelotita>seleccionJ2)){
-                JOptionPane.showMessageDialog(null, "Felicitaciones!" + jugadores[0] +  "\n Has Acertado! \n Punto para " + jugadores[0] , "Juego del Vaso",
-                        JOptionPane.INFORMATION_MESSAGE, Bien);
-                JOptionPane.showMessageDialog(null, "MUY MAL" + jugadores[1], "Juego del Vaso", JOptionPane.INFORMATION_MESSAGE, Mal);
-            }else if(pelotita== seleccionJ2 && (pelotita <seleccionJ1|| pelotita>seleccionJ1)){
-                JOptionPane.showMessageDialog(null, "Felicitaciones!" + jugadores[1] +  "\n Has Acertado! \n Punto para " + jugadores[1] , "Juego del Vaso",
-                        JOptionPane.INFORMATION_MESSAGE, Bien);
-                JOptionPane.showMessageDialog(null, "MUY MAL" + jugadores[0], "Juego del Vaso", JOptionPane.INFORMATION_MESSAGE, Mal);
+        if(modo == 0){
+            System.out.print("contra la computadora");
+        }else{        
+            jugadores[0]= JOptionPane.showInputDialog(null,"JUGADOR Nª1","Juego del Vaso",JOptionPane.QUESTION_MESSAGE).toUpperCase();
+            jugadores[1]= JOptionPane.showInputDialog(null,"JUGADOR Nª2","Juego del Vaso",JOptionPane.QUESTION_MESSAGE).toUpperCase();
+
+            JOptionPane.showMessageDialog(null, "JUGUEMOS!", "Juego del Vaso", JOptionPane.INFORMATION_MESSAGE, Icono);
+
+            for(int i=0; i<contadorVueltas; i++ ){
+
+                //JUGAS DEL JUGADOR 1 ----------------------------------
+            int seleccionJ1 = JOptionPane.showOptionDialog(null,
+                    jugadores[0] + ", \n encuentra la pelotita en los vasos!",
+                    "JUGADOR 1",
+                    JOptionPane.YES_NO_CANCEL_OPTION,
+                    JOptionPane.INFORMATION_MESSAGE, Icono,
+                    new Object[]{"VASO 1", "VASO 2", "VASO 3"}, "VASO1");                
+                jugadasJugador1[i]=seleccionJ1;            
+
+                //ACIERTOS JUGADOR 1
+                if(pelotita == seleccionJ1){
+                    contadorPuntosJ1++;
+                }
+
+                //JUGAS DEL JUGADOR 2 --------------------------------------
+            int seleccionJ2 = JOptionPane.showOptionDialog(null,
+                    jugadores[1] + ", \n encuentra la pelotita en los vasos!",
+                    "JUGADOR 2",
+                    JOptionPane.YES_NO_CANCEL_OPTION,
+                    JOptionPane.INFORMATION_MESSAGE, Icono,
+                    new Object[]{"VASO 1", "VASO 2", "VASO 3"}, "VASO1");                
+                jugadasJugador2[i]=seleccionJ2;   
+
+                //ACIERTOS JUGADOR 2
+                if(pelotita == seleccionJ2){
+                    contadorPuntosJ2++;
+                }
+
+                //NOS FIJAMOS QUIÉN ACERTÓ Y LO MOSTRAMOS POR PANTALLA
+                if(pelotita== seleccionJ1 && pelotita==seleccionJ2){
+                    JOptionPane.showMessageDialog(null, "Felicitaciones! \n Ambos han Acertado! \n Punto para los dos", "Juego del Vaso",
+                            JOptionPane.INFORMATION_MESSAGE, Bien);
+                }else if(pelotita== seleccionJ1 && (pelotita <seleccionJ2|| pelotita>seleccionJ2)){
+                    JOptionPane.showMessageDialog(null, "Felicitaciones!" + jugadores[0] +  "\n Has Acertado! \n Punto para " + jugadores[0] , "Juego del Vaso",
+                            JOptionPane.INFORMATION_MESSAGE, Bien);
+                    JOptionPane.showMessageDialog(null, "MUY MAL" + jugadores[1], "Juego del Vaso", JOptionPane.INFORMATION_MESSAGE, Mal);
+                }else if(pelotita== seleccionJ2 && (pelotita <seleccionJ1|| pelotita>seleccionJ1)){
+                    JOptionPane.showMessageDialog(null, "Felicitaciones!" + jugadores[1] +  "\n Has Acertado! \n Punto para " + jugadores[1] , "Juego del Vaso",
+                            JOptionPane.INFORMATION_MESSAGE, Bien);
+                    JOptionPane.showMessageDialog(null, "MUY MAL" + jugadores[0], "Juego del Vaso", JOptionPane.INFORMATION_MESSAGE, Mal);
+                }else{
+                    JOptionPane.showMessageDialog(null, "Muy mal los dos! \n Si siguen así, les sacaré puntos a ambos", "Juego del Vaso",
+                            JOptionPane.INFORMATION_MESSAGE, Mal);
+                    }            
+               }   
+
+            //MOSTRAMOS LOS PUNTOS
+            JOptionPane.showMessageDialog(null, "REUSLTADO FINAL \n"
+                    + jugadores[0] + ": " + contadorPuntosJ1 + "\n" 
+                    + jugadores[1] + ": " + contadorPuntosJ2 + "\n"
+                    , "Juego del Vaso", JOptionPane.INFORMATION_MESSAGE, Icono);
+
+            //NOS FIJAMOS QUIÉN GANÓ. CONTAMOS LOS PUNTOS.
+            if(contadorPuntosJ1 > contadorPuntosJ2){
+                JOptionPane.showMessageDialog(null, "FELICITCIONES, "+ jugadores[0] + "\n Has Ganado!",
+                        "Juego del Vaso",
+                        JOptionPane.INFORMATION_MESSAGE, Icono);
+            }else if(contadorPuntosJ1 < contadorPuntosJ2){
+                JOptionPane.showMessageDialog(null, "FELICITCIONES, " + jugadores[1] + "\n Has Ganado!",
+                        "Juego del Vaso",
+                        JOptionPane.INFORMATION_MESSAGE, Icono);                
             }else{
-                JOptionPane.showMessageDialog(null, "Muy mal los dos! \n Si siguen así, les sacaré puntos a ambos", "Juego del Vaso",
-                        JOptionPane.INFORMATION_MESSAGE, Mal);
-                }            
-           }   
-           
-        //MOSTRAMOS LOS PUNTOS
-        JOptionPane.showMessageDialog(null, "REUSLTADO FINAL \n"
-                + jugadores[0] + ": " + contadorPuntosJ1 + "\n" 
-                + jugadores[1] + ": " + contadorPuntosJ2 + "\n"
-                , "Juego del Vaso", JOptionPane.INFORMATION_MESSAGE, Icono);
-        
-        //NOS FIJAMOS QUIÉN GANÓ. CONTAMOS LOS PUNTOS.
-        if(contadorPuntosJ1 > contadorPuntosJ2){
-            JOptionPane.showMessageDialog(null, "FELICITCIONES, "+ jugadores[0] + "\n Has Ganado!",
-                    "Juego del Vaso",
-                    JOptionPane.INFORMATION_MESSAGE, Icono);
-        }else if(contadorPuntosJ1 < contadorPuntosJ2){
-            JOptionPane.showMessageDialog(null, "FELICITCIONES, " + jugadores[1] + "\n Has Ganado!",
-                    "Juego del Vaso",
-                    JOptionPane.INFORMATION_MESSAGE, Icono);                
-        }else{
-            JOptionPane.showMessageDialog(null, "Qué triste EMPATE",
-                    "Juego del Vaso",
-                    JOptionPane.INFORMATION_MESSAGE, Icono);
+                JOptionPane.showMessageDialog(null, "Qué triste EMPATE",
+                        "Juego del Vaso",
+                        JOptionPane.INFORMATION_MESSAGE, Icono);
+            }
         }
+        
         
         
         
