a06a6e177b663b91a15beaf655a1954d20fb2969
notsag01 <79101798+notsag01@users.noreply.github.com>
2023-06-12 09:23:40 -0300

    ejercicio2m2

---
+++ ejercicio2m2.vehiculos/src/ejercicio2m2/vehiculos/Ejercicio2m2Vehiculos.java
@@ -0,0 +1,13 @@
+
+package ejercicio2m2.vehiculos;
+
+
+public class Ejercicio2m2Vehiculos {
+
+
+    public static void main(String[] args) {
+        Auto auto1= new Auto();
+        auto1.getData();
+    }
+    
+}
