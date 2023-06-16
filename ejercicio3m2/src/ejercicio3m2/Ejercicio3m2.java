f1cbc714d86562bab2c7db10de013126189f93dc
notsag01 <79101798+notsag01@users.noreply.github.com>
2023-06-16 11:54:10 -0300

    Cuenta Bancaria

--- ejercicio3m2/src/ejercicio3m2/Ejercicio3m2.java
+++ ejercicio3m2/src/ejercicio3m2/Ejercicio3m2.java
@@ -1,13 +1,29 @@
 package ejercicio3m2;
 
+import javax.swing.JOptionPane;
+
 
 public class Ejercicio3m2 {
 
 
     public static void main(String[] args) {
+        String usuario ="";
+        int tipoCuenta;
         
-        CuentaBancaria cuenta1=new CuentaBancaria();
-        cuenta1.getDatos();
+        //INGRESAR NOMBRE DE USUARIO
+        do{
+             usuario=(String) JOptionPane.showInputDialog(null, "Ingrese su nombre", "DATOS", JOptionPane.QUESTION_MESSAGE);
+        }while(usuario == null || usuario.equals("") );
+        
+        tipoCuenta=JOptionPane.showOptionDialog(null,
+                "Tipo de cuenta?",
+                "DATOS",
+                JOptionPane.YES_NO_CANCEL_OPTION,
+                JOptionPane.QUESTION_MESSAGE, null,
+                new Object [] {"CUENTA CORRIENTE", "CAJA DE AHORRO"},
+                "CUENTA CORRIETE");
+        System.out.println(tipoCuenta);
+        
         
         
         
