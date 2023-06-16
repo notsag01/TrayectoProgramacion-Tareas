7b6b96ba19f95e4e88b4f7e6962325976c2b40e5
notsag01 <79101798+notsag01@users.noreply.github.com>
2023-06-16 15:05:49 -0300

    cuentaBancaria

--- ejercicio3m2/src/ejercicio3m2/cuentaCorriente.java
+++ ejercicio3m2/src/ejercicio3m2/cuentaCorriente.java
@@ -18,12 +18,31 @@ public class cuentaCorriente {
     public cuentaCorriente (String usuarios){
         this.usuarios = usuarios;
     }
-    
+    public void finalizar(){
+        System.exit(0);
+    }
+    public void nuevaOperacion(){
+        int opciones = JOptionPane.showOptionDialog(null,
+                "DESEA REALIZAR OTRA OPERACION?",
+                "CONSULTA",
+                JOptionPane.YES_NO_CANCEL_OPTION,
+                JOptionPane.QUESTION_MESSAGE, null,
+                new Object []{"DEPOSITAR", "DEBITAR", "SALIR"}, "DEPOSITAR");
+        
+        if(opciones == 0){
+            acreditar();
+        }else if(opciones == 1){
+            debitar();
+        }else{
+            finalizar();
+        }
+    }
     public void saludar(){
         JOptionPane.showMessageDialog(null, "Bienvenido, " + usuarios, "SALUDO", JOptionPane.INFORMATION_MESSAGE);
     }
     public void informarSaldo(){
-        JOptionPane.showMessageDialog(null, "Su saldo es de $" + saldo, "SALDO", JOptionPane.INFORMATION_MESSAGE);        
+        JOptionPane.showMessageDialog(null, "Su saldo es de $" + saldo, "SALDO", JOptionPane.INFORMATION_MESSAGE);   
+        nuevaOperacion();
     }
     public void solicitarAccion(){
         int accion = JOptionPane.showOptionDialog(null,
@@ -45,7 +64,22 @@ public class cuentaCorriente {
         saldo += acrSaldo;
         informarSaldo();
     }
-    public void debitar (){}
+    public void debitar (){
+        if(saldo <= 0){
+            JOptionPane.showMessageDialog(null, "Ustes no dispone de sufieiciente dinero en este momento", "INFORMACION", JOptionPane.INFORMATION_MESSAGE, null);
+            nuevaOperacion();
+        }else{
+            String sustraerSaldo = (String) JOptionPane.showInputDialog(null, "Cual es el monto a depositar?", "DEPOSITO", JOptionPane.QUESTION_MESSAGE);
+            int debSaldo= Integer.parseInt(sustraerSaldo);
+            if(saldo >= debSaldo){
+                saldo -= debSaldo;
+                informarSaldo();                    
+            }else{            
+                JOptionPane.showMessageDialog(null, "Ustes no dispone de sufieiciente dinero en este momento", "INFORMACION", JOptionPane.INFORMATION_MESSAGE, null);
+                nuevaOperacion();
+            }
+        }
+    }
     
     
     
