e0cb715af2297bcfb25c4dc8451bb755f7fdf86b
notsag01 <gcordoba1981@gmail.com>
2023-06-19 23:50:58 -0300

    ejercicio4m2

--- ejercicio4m3/src/ejercicio4m3/Ejercicio4m3.java
+++ ejercicio4m3/src/ejercicio4m3/Ejercicio4m3.java
@@ -4,12 +4,45 @@
  */
 package ejercicio4m3;
 
+import javax.swing.JOptionPane;
+
 public class Ejercicio4m3 {
 
     public static void main(String[] args) {
         Autos auto =new Autos("","","");
-        auto.getDatos();
-        auto.imprimir();
+        
+        int nuevo=0;
+        while(nuevo == 0){
+            int seleccion = JOptionPane.showOptionDialog(null, "Qué desea cargar?", "Nueva Cargar?",
+                    JOptionPane.YES_NO_CANCEL_OPTION,
+                    JOptionPane.QUESTION_MESSAGE, null,
+                    new Object[]{"AUTOS", "MOTOS"}, "AUTOS");
+
+            switch(seleccion){
+                case 0: auto.getDatos();
+                        auto.imprimir();
+                break;
+                case 1: 
+                    //MOTOS!!!!!!!!!!!!!!!!!!
+                break;        
+            }
+            
+            int nuevaConsulta = JOptionPane.showOptionDialog(null, "Desea realizar una nueva carga?", "Nueva Cargar?",
+                    JOptionPane.YES_NO_CANCEL_OPTION,
+                    JOptionPane.QUESTION_MESSAGE, null,
+                    new Object[]{"SI", "NO"}, "SI");
+            
+            switch(nuevaConsulta){
+                case 0: nuevo = 0;
+                break;
+                case 1: nuevo = 1;
+            }
+            
+        }
+        
+        
+        
+        
     }
     
 }
