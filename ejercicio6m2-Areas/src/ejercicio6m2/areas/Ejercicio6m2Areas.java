98e1f990a00bd3ef4fc3e82cbcac069b01538416
notsag01 <79101798+notsag01@users.noreply.github.com>
2023-06-21 09:31:13 -0300

    ejercicio6m2

---
+++ ejercicio6m2-Areas/src/ejercicio6m2/areas/Ejercicio6m2Areas.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio6m2.areas;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Ejercicio6m2Areas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ ejercicio6m2-Areas/src/ejercicio6m2/areas/Funciones.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio6m2.areas;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Funciones {
+    private String base, altura;
+    
+    //CONSTRUCTOR
+    public 
+}
