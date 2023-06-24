5f66b63026209a391b251c339969d573d5fdfdf3
notsag01 <gcordoba1981@gmail.com>
2023-06-24 19:16:30 -0300

    Polimorfismo

---
+++ ejercicio14m2-polimorfismo/src/ejercicio14m2/polimorfismo/Moto.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio14m2.polimorfismo;
+
+/**
+ *
+ * @author gas12
+ */
+public class Moto extends Vehiculos {
+
+    @Override
+    public void estacionar() {
+        System.out.println("****   INSTRUCCIONES PARA ESTACIONAR UNA MOTO   ****");
+        System.out.println("****   Encuentra el espacion indicado." );
+        System.out.println("****   Señalice su intención. ");
+        System.out.println("****   Posicionate cerca del espacio. ");
+        System.out.println("****   Posicionate cerca del espacio. ");
+        System.out.println("****   Deten la moto paralela y coloca tu pie en el suelo para mantener el equilibrio. ");
+        System.out.println("****   Utiliza el caballete lateral para estabilizar la moto y mantenerla en posción vertical. ");
+        System.out.println("****   Apaga el motor y asegurla. ");
+        System.out.println("****   Verifica el espacio, que no sea una obstrucción. ");
+    }
+    
+}
