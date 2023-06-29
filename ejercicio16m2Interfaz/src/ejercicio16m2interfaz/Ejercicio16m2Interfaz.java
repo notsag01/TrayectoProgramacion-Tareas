a1b44e093fada856a96d36bf67c7c0489ad81303
notsag01 <79101798+notsag01@users.noreply.github.com>
2023-06-29 12:37:13 -0300

    Interfaz

--- ejercicio16m2Interfaz/src/ejercicio16m2interfaz/Ejercicio16m2Interfaz.java
+++ ejercicio16m2Interfaz/src/ejercicio16m2interfaz/Ejercicio16m2Interfaz.java
@@ -12,7 +12,21 @@ public class Ejercicio16m2Interfaz extends JFrame {
     public Ejercicio16m2Interfaz(){
         setLayout(null);
         
+        etiqueta=new JLabel("Nombre y Apellidio: Cordoba Gastón H.");
+        etiqueta.setBounds(0, 0, 600, 100);
+        add(etiqueta);
         
+        etiqueta=new JLabel("Teléfono: 15-1234-5678");
+        etiqueta.setBounds(0, 20, 600, 100);
+        add(etiqueta);
+        
+        etiqueta=new JLabel("Mail: gcordoba1981@gmail.com");
+        etiqueta.setBounds(0, 40, 600, 100);
+        add(etiqueta);
+        
+        etiqueta=new JLabel("Dni: 29.111.111");
+        etiqueta.setBounds(0, 60, 600, 100);
+        add(etiqueta);
     }
 
 
