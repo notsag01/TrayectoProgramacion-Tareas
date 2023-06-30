504f5d2b574bffab0af07902dc43a5bb5701210e
notsag01 <79101798+notsag01@users.noreply.github.com>
2023-06-30 17:08:14 -0300

    Interfaz-Botones-Eventos

--- ejercicio17m2InterfazBotones/src/ejercicio17m2interfazbotones/Ejercicio17m2InterfazBotones.java
+++ ejercicio17m2InterfazBotones/src/ejercicio17m2interfazbotones/Ejercicio17m2InterfazBotones.java
@@ -14,30 +14,21 @@ public class Ejercicio17m2InterfazBotones extends JFrame implements ActionListen
     //CONSTRUCTOR
     public Ejercicio17m2InterfazBotones(){
         btn1=new JButton("btn1");
-        btn1.setBounds(10, 50, 100, 25);
+        btn1.setBounds(0, 100, 100, 50);
         btn1.addActionListener(this);
         add(btn1);
         btn2=new JButton("btn2");
-        btn2.setBounds(20, 50, 100, 25);
+        btn2.setBounds(200, 100, 100, 50);
         btn2.addActionListener(this);
         add(btn2);
         btn3=new JButton("btn3");
-        btn3.setBounds(30, 50, 100, 25);
+        btn3.setBounds(300, 100, 100, 50);
+        btn3.addActionListener(this);
         add(btn3);
         
         etiqueta=new JLabel("Presiona un boton");
         etiqueta.setBounds(0, 400, 200, 50);
     }
-
-    public static void main(String[] args) {
-        Ejercicio17m2InterfazBotones formulario=new Ejercicio17m2InterfazBotones();
-        formulario.setBounds(0, 0, 600, 400);
-        formulario.setVisible(true);
-        formulario.setResizable(false);
-        formulario.setLocationRelativeTo(null);
-        formulario.setTitle("BOTONES");
-    }
-
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource()==btn1){
@@ -48,5 +39,17 @@ public class Ejercicio17m2InterfazBotones extends JFrame implements ActionListen
             etiqueta.setText("Ha apretado el botón 3");
         }
     }
+
+    public static void main(String[] args) {
+        Ejercicio17m2InterfazBotones formulario=new Ejercicio17m2InterfazBotones();
+        formulario.setBounds(0, 0, 600, 400);
+        formulario.setVisible(true);
+        formulario.setResizable(false);
+        formulario.setLocationRelativeTo(null);
+        formulario.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        formulario.setTitle("BOTONES");
+    }
+
+    
     
 }
