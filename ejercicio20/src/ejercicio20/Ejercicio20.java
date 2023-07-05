5f42656e85d68ab64915895875fc292aac449a4e
notsag01 <gcordoba1981@gmail.com>
2023-07-05 19:54:21 -0300

    Formularios Promedios

--- ejercicio20/src/ejercicio20/Ejercicio20.java
+++ ejercicio20/src/ejercicio20/Ejercicio20.java
@@ -3,6 +3,7 @@ package ejercicio20;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -18,36 +19,37 @@ public class Ejercicio20 extends JFrame implements ActionListener {
         setLayout(null);
         
         etiq1=new JLabel("Inserte la nota numero 1: ");
-        etiq1.setBounds(0, 100, 200, 20);
+        etiq1.setBounds(50, 100, 200, 40);
         add(etiq1);
         
         etiq2=new JLabel("Inserte la nota numero 2: ");
-        etiq2.setBounds(0, 150, 200, 20);
+        etiq2.setBounds(50, 150, 200, 20);
         add(etiq2);
         
         etiq3=new JLabel("Inserte la nota numero 3: ");
-        etiq3.setBounds(0, 200, 200, 20);
+        etiq3.setBounds(50, 200, 200, 20);
         add(etiq3);
         
         input1=new JTextField();
-        input1.setBounds(210, 105, 50, 20);
+        input1.setBounds(210, 110, 150, 20);
         add(input1);
         
         input2=new JTextField();
-        input2.setBounds(210, 155, 50, 20);
+        input2.setBounds(210, 160, 150, 20);
         add(input2);
         
         input3=new JTextField();
-        input3.setBounds(210, 205, 50, 20);
+        input3.setBounds(210, 205, 150, 20);
         add(input3);
         
         btn1= new JButton("Calcular");
-        btn1.setBounds(210, 230, 50, 40);
+        btn1.setBounds(210, 250, 150, 45);
         btn1.addActionListener(this);
         add(btn1);
         
         respuesta=new JTextField();
-        respuesta.setBounds(210, 260,50, 15);
+        respuesta.setBounds(50, 320,400, 40);
+        respuesta.setHorizontalAlignment(JTextField.CENTER);
         add(respuesta);
     }
     
@@ -56,12 +58,13 @@ public class Ejercicio20 extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         if(e.getSource()==btn1){
             //System.out.println("Its work");
+            DecimalFormat formato = new DecimalFormat("#.##");
             double nota1=Integer.parseInt(input1.getText());
             double nota2=Integer.parseInt(input2.getText());
             double nota3=Integer.parseInt(input3.getText());
             double promedio=(nota1 + nota2 + nota3) /3;
             
-            respuesta.setText(String.valueOf(promedio));
+            respuesta.setText(String.valueOf(formato.format(promedio)));
             
             
         }
