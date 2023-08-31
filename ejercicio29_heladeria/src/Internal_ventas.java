5d5d36e4d739390b9cd7046e8e8f013995cb3923
notsag01 <79101798+notsag01@users.noreply.github.com>
2023-08-31 17:14:14 -0300

    Heladeria

--- ejercicio29_heladeria/src/Internal_ventas.java
+++ ejercicio29_heladeria/src/Internal_ventas.java
@@ -10,6 +10,9 @@ import javax.swing.JOptionPane;
 public class Internal_ventas extends javax.swing.JInternalFrame {
         int cantidad=0;
         int gustosPermitidos=0;
+        
+        String fecha, nombre, mail, tamaño;
+        String gustos[];
 
     public Internal_ventas() {
         initComponents();
@@ -27,7 +30,12 @@ public class Internal_ventas extends javax.swing.JInternalFrame {
         String fecha=sdf.format(date);
         jTextField_fecha.setText(fecha);        
     }
-    
+    private void getDatos(){
+        fecha = jTextField_fecha.getText();
+        nombre = jTextField_nombre.getText();
+        mail = jTextField_mail.getText();
+        
+    }
     private void checkCantidad(){
         if(cantidad==gustosPermitidos){
             Component [] componentes = jPanel_gustos.getComponents();      
@@ -73,8 +81,8 @@ public class Internal_ventas extends javax.swing.JInternalFrame {
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
-        jTextField2 = new javax.swing.JTextField();
-        jTextField3 = new javax.swing.JTextField();
+        jTextField_nombre = new javax.swing.JTextField();
+        jTextField_mail = new javax.swing.JTextField();
         jPanel_tamaños = new javax.swing.JPanel();
         jPanel_gustos = new javax.swing.JPanel();
         jCheckBox_chocolate = new javax.swing.JCheckBox();
@@ -150,8 +158,8 @@ public class Internal_ventas extends javax.swing.JInternalFrame {
                     .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 66, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(33, 33, 33)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jTextField3)
-                    .addComponent(jTextField2)
+                    .addComponent(jTextField_mail)
+                    .addComponent(jTextField_nombre)
                     .addGroup(jPanel2Layout.createSequentialGroup()
                         .addComponent(jTextField_fecha, javax.swing.GroupLayout.PREFERRED_SIZE, 113, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(0, 137, Short.MAX_VALUE)))
@@ -167,11 +175,11 @@ public class Internal_ventas extends javax.swing.JInternalFrame {
                 .addGap(18, 18, 18)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel3)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jTextField_nombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(21, 21, 21)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel4)
-                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jTextField_mail, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap())
         );
 
@@ -522,6 +530,7 @@ public class Internal_ventas extends javax.swing.JInternalFrame {
     private void jRadioButton_kiloActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton_kiloActionPerformed
         if(jRadioButton_kilo.isSelected()){
             gustosPermitidos=4;
+            tamaño="Kilo";
             JOptionPane.showMessageDialog(null, "Solo puede elegir 4 Gustos.");
         }
         jRadioButton_cono.setSelected(false);
@@ -612,6 +621,7 @@ public class Internal_ventas extends javax.swing.JInternalFrame {
     private void jRadioButton_conoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton_conoActionPerformed
         if(jRadioButton_cono.isSelected()){
             gustosPermitidos=2;
+            tamaño="Cono";
             JOptionPane.showMessageDialog(null, "Solo puede elegir 2 Gustos.");
         }
         jRadioButton_cuartoKilo.setSelected(false);
@@ -622,6 +632,7 @@ public class Internal_ventas extends javax.swing.JInternalFrame {
     private void jRadioButton_cuartoKiloActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton_cuartoKiloActionPerformed
         if(jRadioButton_cuartoKilo.isSelected()){
             gustosPermitidos=2;
+            tamaño="Cuarto Kilo";
             JOptionPane.showMessageDialog(null, "Solo puede elegir 2 Gustos.");
         }
         jRadioButton_cono.setSelected(false);
@@ -632,6 +643,7 @@ public class Internal_ventas extends javax.swing.JInternalFrame {
     private void jRadioButton_medioKiloActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton_medioKiloActionPerformed
         if(jRadioButton_medioKilo.isSelected()){
             gustosPermitidos=3;
+            tamaño="Medio Kilo";
             JOptionPane.showMessageDialog(null, "Solo puede elegir 3 Gustos.");
         }
         jRadioButton_cono.setSelected(false);
@@ -653,7 +665,7 @@ public class Internal_ventas extends javax.swing.JInternalFrame {
     }//GEN-LAST:event_jButton_deliberyActionPerformed
 
     private void jButton_facturarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_facturarActionPerformed
-        // TODO add your handling code here:
+      
     }//GEN-LAST:event_jButton_facturarActionPerformed
 
 
@@ -689,8 +701,8 @@ public class Internal_ventas extends javax.swing.JInternalFrame {
     private javax.swing.JRadioButton jRadioButton_medioKilo;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
     private javax.swing.JTextField jTextField_fecha;
+    private javax.swing.JTextField jTextField_mail;
+    private javax.swing.JTextField jTextField_nombre;
     // End of variables declaration//GEN-END:variables
 }
