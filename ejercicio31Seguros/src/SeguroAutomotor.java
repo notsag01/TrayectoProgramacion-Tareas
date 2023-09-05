9b088138630790500274c9b0f3db1f82915aaa86
notsag01 <79101798+notsag01@users.noreply.github.com>
2023-09-05 15:50:25 -0300

    Seguros

---
+++ ejercicio31Seguros/src/SeguroAutomotor.java
@@ -0,0 +1,63 @@
+
+import java.util.ArrayList;
+
+
+public class SeguroAutomotor {
+    String id,nombre,telefono,mail,dominio,marcaSelec,modeloSelec,anio;
+    ArrayList <String> coberturaVehiculo = new ArrayList<>();
+    
+    public SeguroAutomotor(String id,
+                            String nombre,
+                            String telefono,
+                            String mail,
+                            String dominio,
+                            String marcaSelec,
+                            String modeloSelec,
+                            String anio){
+        this.id=id;
+        this.nombre=nombre;
+        this.telefono=telefono;
+        this.mail=mail;
+        this.dominio=dominio;
+        this.marcaSelec=marcaSelec;
+        this.modeloSelec=modeloSelec;
+        this.anio=anio;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public String getDominio() {
+        return dominio;
+    }
+
+    public String getMarcaSelec() {
+        return marcaSelec;
+    }
+
+    public String getModeloSelec() {
+        return modeloSelec;
+    }
+
+    public String getAnio() {
+        return anio;
+    }
+
+    public ArrayList<String> getCoberturaVehiculo() {
+        return coberturaVehiculo;
+    }
+    
+}
