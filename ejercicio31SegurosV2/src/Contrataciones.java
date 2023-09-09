edb9c421d1b879b6a089e50498024583d7ca0fc3
notsag01 <gcordoba1981@gmail.com>
2023-09-08 21:17:17 -0300

    Seguros

--- ejercicio31SegurosV2/src/Contrataciones.java
+++ ejercicio31SegurosV2/src/Contrataciones.java
@@ -31,7 +31,7 @@ public class Contrataciones extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 637, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
